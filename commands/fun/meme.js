107e3e4ff1e99c83223fca9889b5b9a8e62897f4
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-05-22 19:55:19 +0700

    Add files via upload

---
+++ commands/fun/meme.js
@@ -0,0 +1,22 @@
+module.exports = ({
+    name: "meme",
+    code: `$djseval[(async()=>{
+   channel.startTyping()
+   const Discord = require('discord.js')
+   const fetch = require('node-fetch')
+   const subReddits = ["dankmeme", "meme", "memes"\\];
+   const random = Math.floor(Math.random() * subReddits.length)
+   var body = await fetch('https://www.reddit.com/r/' + subReddits[random\\] + '/random/.json')
+   body = await body.json()
+   
+   const a = body[0\\]
+   const embed = new Discord.MessageEmbed()
+   .setTitle(a.data.children[0\\].data.title)
+   .setURL('https://reddit.com'+a.data.children[0\\].data.permalink)
+   .setColor('RANDOM')
+   .setImage(a.data.children[0\\].data.url_overridden_by_dest)
+   .setFooter(':thumbsup: ' + a.data.children[0\\].data.ups + ' :speech_balloon: ' + a.data.children[0\\].data.num_comments + ' - ' + a.data.children[0\\].data.subreddit_name_prefixed);
+   message.channel.send(embed)
+   channel.stopTyping()
+   })()]`
+   })
