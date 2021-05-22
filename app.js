e76abed8e49b9b9075b0196e6a367e3319ad4ead
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-05-22 20:29:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const port = 18273;
 
-app.get('/', (req, res) => res.send('Yurika ON'));
+app.get('/', (req, res) => res.send('Aoi.js'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
